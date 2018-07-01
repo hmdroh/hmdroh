31e2c3c4d1e50d21114115cf03b53c04d615ebe7
Hamed Rohani <hamedrohani@Hameds-MacBook-Pro.local>
2018-06-30 19:12:55 -0700

    new

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+
+import Application from "./components/App";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Hamed Rohani's React App</h2>
-        </div>
-        <p className="App-intro">
-          Welcome !
-        </p>
+      <div>
+
+      <Application />
       </div>
     );
   }
---
+++ client/src/components/App.js
@@ -0,0 +1,32 @@
+import React, { Component, Route } from "react";
+import logo from "../logo.svg";
+import "./App.css";
+
+class Application1 extends Component {
+    render() {
+
+
+        return (
+
+            <div>
+                <div class="div1">
+
+                    <div className="App">
+                        <div className="App-header">
+                            <img src={logo} className="App-logo" alt="logo" />
+                            <h2>Hamed Rohani</h2>
+                        </div>
+                        <p className="App-intro">
+                            Full Stack Web Developer
+                        </p>
+                        <div class="lnk"><a href="https://linkedin.com/in/hmdroh">Linkedin</a></div>
+                        <div class="lnk"><a href="https://github.com/hmdroh">Github</a></div>
+                    </div>
+                </div>
+                {/* <div class="div2">Div 2</div> */}
+            </div>
+        )
+    }
+}
+
+export default Application1;
